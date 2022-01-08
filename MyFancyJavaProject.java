8dad4768337b1be0038b5b83ba785b5ce771dc80
TakiYasirUIU <yasirtaki1995@gmail.com>
2022-01-08 14:33:00 +0600

    Singleton pattern update

--- MyFancyJavaProject.java
+++ MyFancyJavaProject.java
@@ -1,4 +1,47 @@
-taki will write code here
+class PatternDecorator implements Shape
+{
+	Shape shape;
+	
+	 public PatternDecorator(Shape shape) {
+		
+		this.shape = shape;
+	}
+
+	@Override
+	public void draw()
+	{
+		
+		 this.shape.draw();
+		 addPattern();
+	}
+	
+	private void addPattern() {
+		System.out.println("Pattern is added to the shape");
+	}
+}
+
+class ColorDecorator implements Shape
+{
+	Shape shape;
+	
+	 public ColorDecorator(Shape shape) {
+		
+		this.shape = shape;
+	}
+
+	@Override
+	public void draw()
+	{
+		
+		 this.shape.draw();
+		 fillColor();
+	}
+	
+	private void fillColor() {
+		System.out.println("Color is filled to the shape");
+	}
+}
+
 class Client
 {
 	 public static void main(String[] args) throws Exception
